aba565a1e290df62f77625d21936f76f086d801e
Павел Никуленок <pashaaaa86@gmail.com>
2023-12-01 22:25:29 +0500

    Зарегестрировал User

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('username', 'email')
+    search_fields = ('username', 'email')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
