b42b7d19cf5fd5f825e73130e8e7295471e6a4f6
Павел Никуленок <pashaaaa86@gmail.com>
2023-12-02 15:13:31 +0500

    Начал работу над моделью recipes

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(
+        'Название тега',
+        max_length=25,
+        unique=True,
+        help_text='Название тега'
+    )
+    color = models.CharField(
+        'Цвет',
+        max_length=7,
+        default='#000000',
+        null=True,
+        blank=True,
+        unique=True,
+        verbose_name='Цвет тега',
+        help_text='Цвет тега'
+    )
+    slug = models.SlugField(
+        'Слаг',
+        max_length=25,
+        unique=True,
+    )
+
+    class Meta:
+        ordering = ('slug',)
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['slug'],
+                name='unique_tag'
+            )
+        ]
+
+    def __str__(self):
+        return self.name
+
