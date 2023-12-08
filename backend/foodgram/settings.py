a74e152c927d3fcdb0cf013da26beb5c3e90bde0
Павел Никуленок <pashaaaa86@gmail.com>
2023-12-08 18:31:01 +0500

    fixx

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,9 +8,9 @@ load_dotenv(find_dotenv())
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
-DEBUG = True
+DEBUG = os.getenv('DEBUG_VALUE') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = str(os.getenv("DJANGO_ALLOWED_HOSTS")).split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
